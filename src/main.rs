dd04a52113dab1d4b754c658b8303a7068ae5c57
Mvmo <mvmogo@gmail.com>
2023-10-11 01:24:28 +0200

    feat: terminal setup and clear

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::io::stdout;
 
+use ratatui::{prelude::CrosstermBackend, Terminal};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.clear()?;
+
+    Ok(())
 }
