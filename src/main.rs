297df97259344ca88522e402ac5e3dc48c0238f1
Mvmo <mvmogo@gmail.com>
2023-10-11 02:37:17 +0200

    feat: tuirenderable trait + implement it for window
    + TuiRenderable Trait
    + impl TuiRenderable for Window
    + some examples

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{stdout, Stdout}, time::Duration, sync::mpsc::{self, Receiver}};
+use std::{io::{stdout, Stdout}, time::Duration, sync::mpsc::{self, Receiver}, fmt::Display};
 
 use crossterm::{
     cursor::SetCursorStyle,
@@ -6,14 +6,16 @@ use crossterm::{
 };
 
 use ratatui::{
-    prelude::CrosstermBackend,
-    widgets::Block
+    prelude::{CrosstermBackend, Rect},
+    widgets::{Block, Paragraph}
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 type Backend = CrosstermBackend<Stdout>;
 type Terminal = ratatui::Terminal<Backend>;
+type Frame<'a> = ratatui::Frame<'a, Backend>;
+
 struct EsenixContext {
     mode: EsenixMode,
     windows: Vec<Window>,
@@ -55,6 +57,30 @@ impl Default for Window {
     }
 }
 
+trait TuiRenderable {
+    fn render(&self, context: &EsenixContext, frame: &mut Frame, area: Rect);
+}
+
+impl TuiRenderable for Window {
+    fn render(&self, context: &EsenixContext, frame: &mut Frame, area: Rect) {
+        match self.buffer_idx {
+            Some(idx) => {
+                let buffer = context.buffers.get(idx).unwrap();
+                if let Buffer::Text { name, content } = buffer {
+                    let p = Paragraph::new(format!("{name} --> {content}"));
+                    frame.render_widget(p, area);
+                }
+            }
+            None => {
+                context.buffers.len();
+                let p = Paragraph::new(format!("Open Buffers: {}", context.buffers.len()));
+
+                frame.render_widget(p, area);
+            }
+        }
+    }
+}
+
 fn start_crossterm_event_polling_thread() -> Receiver<KeyEvent> {
     let (tx, rx) = mpsc::channel();
     std::thread::spawn(move || {
@@ -74,6 +100,18 @@ fn start_crossterm_event_polling_thread() -> Receiver<KeyEvent> {
 }
 
 fn main() -> Result<()> {
+    let mut context = EsenixContext::default();
+
+    //* push example buffer
+    context.buffers.push(
+        Buffer::Text {
+            name: String::from("example"),
+            content: String::from("hallo, welt")
+        }
+    );
+
+    context.windows.push(Window::default());
+
     let backend = Backend::new(stdout());
     let mut terminal = Terminal::new(backend)?;
 
@@ -89,15 +127,18 @@ fn main() -> Result<()> {
         if let Ok(key_event) = key_event_receiver.try_recv() {
             match key_event.code {
                 KeyCode::Esc => break,
+                KeyCode::Tab => {
+                    context.windows.first_mut()
+                        .unwrap()
+                        .buffer_idx = Some(0);
+                }
                 _ => {}
             }
         }
 
         terminal.draw(|frame| {
-            let block = Block::default()
-                .title("hallo");
-
-            frame.render_widget(block, frame.size());
+            context.windows.iter()
+                .for_each(|window| window.render(&context, frame, frame.size()))
         })?;
     }
 
